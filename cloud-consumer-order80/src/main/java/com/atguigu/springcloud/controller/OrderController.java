b7cbbbc661731b87739554afc692caf283c7f386
悄悄拔尖 <2549835695@qq.com>
2023-09-20 09:31:19 +0800

    2023.9.20

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -18,7 +18,7 @@ import java.util.List;
 
 @RestController
 public class OrderController {
-//    public static final String PAYMENT_URL = "http://localhost:8001";
+    //    public static final String PAYMENT_URL = "http://localhost:8001";
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
@@ -33,21 +33,28 @@ public class OrderController {
     }
 
     @GetMapping("/consumer/payment/create")
-    public CommonResult create( Payment payment) {
+    public CommonResult create(Payment payment) {
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
+
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult getPayment(@PathVariable("id") Long id) {
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
     }
+
     @GetMapping(value = "/consumer/payment/lb")
-public  String getPaymentLB(){
+    public String getPaymentLB() {
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
-        if(instances==null||instances.size()<=0){
-            return  null;
+        if (instances == null || instances.size() <= 0) {
+            return null;
         }
-        ServiceInstance serviceInstance= loadBalance.instance(instances);
+        ServiceInstance serviceInstance = loadBalance.instance(instances);
         URI uri = serviceInstance.getUri();
-        return  restTemplate.getForObject(uri+"/payment/lb",String.class);
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
+    }
+    @GetMapping("/consumer/payment/zipkin")
+    public String paymentZipKin(){
+        String result=restTemplate.getForObject("http://localhost:8001"+"/payment/zipkin/",String.class);
+        return  result;
     }
 }
